7e5120437d7757e5ee4fdffea5fb6f9a03d94db8
Guilherme Messias <guiihh532@gmail.com>
2021-02-09 18:11:27 -0300

    delete implmentation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,7 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();//injeção de dependencia sem explicitar a implementação
 		System.out.println("=== TEST 1: seller findyById ======");
@@ -42,5 +43,14 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed ");
+		
+		System.out.println("\n=== TEST 6: seller DELETE ======");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.print("Delete Completed");
+		
+		sc.close();
+
 	}
 }
