6058adf572df5d99807f53c3c8513d0f19d9a560
Guilherme Messias <guiihh532@gmail.com>
2021-02-09 14:54:43 -0300

    FyndById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,11 +12,12 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();//injeção de dependencia sem explicitar a implementação
+		Seller seller = sellerDao.findById(8);
 		
 		System.out.println(seller);
+		
 		
 	}
 
